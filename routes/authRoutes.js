a2395ff8270b2d2729a75e592b8e4e71a6876819
pgrdev01 <100864942+pgrdev01@users.noreply.github.com>
2026-07-07 23:13:29 +0530

    auth route test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,4 +3,8 @@ const router =express.Router( )
 const authController = require('../controllers/authController')
 router.post("/signup",authController.signup)
 router.post("/login",authController.login)
+
+router.get('/test', (req, res) => {
+  res.json({ message: 'Auth route working' });
+});
 module.exports =router
